93d59fbef37bf1e24056f1e130127e52cc7116d6
Joshua <joshuagoldman94@gmail.com>
2024-04-22 12:24:34 +0300

    small adjustments prior to testing + added test

---
+++ src/embedded_resources/exmple_package.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE PACKAGE my_package
+IS
+  PROCEDURE proc1 (
+    p_in_param1  IN  VARCHAR2,
+    p_in_param2  IN  NUMBER,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc2 (
+    p_in_param   IN  NUMBER,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc3 (
+    p_in_param1  IN  VARCHAR,
+    p_in_param2  IN  VARCHAR2,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc4 (
+    p_in_param   IN  NUMBER,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc5 (
+    p_in_param1  IN  VARCHAR2,
+    p_in_param2  IN  NUMBER,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc6 (
+    p_in_param   IN  VARCHAR,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc7 (
+    p_in_param1  IN  NUMBER,
+    p_in_param2  IN  VARCHAR2,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc8 (
+    p_in_param   IN  VARCHAR2,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc9 (
+    p_in_param1  IN  VARCHAR,
+    p_in_param2  IN  VARCHAR,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+
+  PROCEDURE proc10 (
+    p_in_param   IN  NUMBER,
+    p_out_cursor OUT SYS_REFCURSOR
+  );
+END my_package;
+/
