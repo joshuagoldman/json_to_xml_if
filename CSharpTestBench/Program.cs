94efb1da43b62ae3c028c18439c38d23ad5adb6d
Joshua <joshuagoldman94@gmail.com>
2024-04-23 11:56:25 +0300

    Addedto test bench

--- CSharpTestBench/Program.cs
+++ CSharpTestBench/Program.cs
@@ -12,32 +12,43 @@ class Program
   [DllImport("json_xml_parser", EntryPoint = "xml_to_json", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
   static extern String XmlToJson(string xml_str, bool to_camel_case);
 
+  [System.Security.SuppressUnmanagedCodeSecurity]
+  [DllImport("json_xml_parser", EntryPoint = "stored_procedure_to_json", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+  static extern String StoredProcedureToJson(string plSqlPackageStr);
+
   static void Main(string[] args)
   {
 
     var assembly = Assembly.GetExecutingAssembly();
     using var resourceJson = assembly.GetManifestResourceStream("CSharpTestBench.EmbeddedResources.json_example.json");
     using var resourceXml = assembly.GetManifestResourceStream("CSharpTestBench.EmbeddedResources.xml_example.xml");
+    using var resourcePlSqlPackage = assembly.GetManifestResourceStream("CSharpTestBench.EmbeddedResources.xml_example.xml");
     using var srJson = new StreamReader(resourceJson);
     string jsonContent = srJson.ReadToEnd();
     using var srXml = new StreamReader(resourceXml);
     string xmlContent = srXml.ReadToEnd();
+    using var srPlSqlPackage = new StreamReader(resourcePlSqlPackage);
+    string plSqlPackageContent = srPlSqlPackage.ReadToEnd();
 
     try
     {
       var res_json = XmlToJson(xmlContent, true);
       var res_xml = JsonToXml(jsonContent, true, "parameters");
+      var res_pl_sql_package = StoredProcedureToJson(plSqlPackageContent);
 
+      string basePath = String.Empty;
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+      {
+        basePath = "/home/joshua/Public/Tests";
+      }
+      else
+      {
+        basePath = "C:/Data";
+      }
 
-#if Windows
-      File.WriteAllText("C:/Data/xml_run_fr_csharp.xml", res_xml);
-      File.WriteAllText("C:/Data/joshua/Public/Tests/json_run_fr_csharp.json", res_json);
-#else 
-      File.WriteAllText("/home/joshua/Public/Tests/xml_run_fr_csharp.xml", res_xml);
-      File.WriteAllText("/home/joshua/Public/Tests/json_run_fr_csharp.json", res_json);
-
-#endif
-
+      File.WriteAllText($"{basePath}/xml_run_fr_csharp.xml", res_xml);
+      File.WriteAllText($"{basePath}/json_run_fr_csharp.json", res_json);
+      File.WriteAllText($"{basePath}/pl_sql_json_run_fr_csharp.json", res_json);
     }
     catch (System.Exception e)
     {
